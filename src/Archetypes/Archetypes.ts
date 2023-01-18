c6a4cebd0d00ca1c7f76af067fb71b2427dbb3d1
karl-no <karl.publicidade@gmail.com>
2023-01-18 18:48:43 -0300

    feat: class Archetypes - Requisito 4

---
+++ src/Archetypes/Archetypes.ts
@@ -0,0 +1,25 @@
+import { EnergyType } from '../Energy';
+
+abstract class Archetypes {
+  constructor(
+    private _name: string,
+    private _special: number = 0,
+    private _cost: number = 0,
+  ) { }
+
+  get name() {
+    return this._name;
+  }
+
+  get special() {
+    return this._special;
+  }
+
+  get cost() {
+    return this._cost;
+  }
+
+  abstract get energyType(): EnergyType;
+}
+
+export default Archetypes;
